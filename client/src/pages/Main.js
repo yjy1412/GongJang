16d293f46fd4a1d5b6a98b8238d4aeb8c9e49ff3
treatme030 <treatme8192@gmail.com>
2021-11-26 22:02:54 +0900

    feat : Main 페이지 목업 디자인

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+import ItemList from '../components/main/ItemList';
+import GameImg from '../components/main/GameImg';
+
+const MainBlock = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  .share-text {
+    position: relative;
+    text-align: center;
+    padding: 2rem 0;
+    &:after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 35%;
+      height: 2px;
+      width: 50px;
+      background: #575f95;
+    }
+    &:before {
+      content: '';
+      position: absolute;
+      top: 50%;
+      right: 35%;
+      height: 2px;
+      width: 50px;
+      background: #575f95;
+    }
+  }
+`;
 
 const Main = () => {
   return (
-    <div>
-      <h1>Main 페이지 입니다.</h1>
-    </div>
+    <MainBlock>
+      <GameImg/>
+      <div className="share-text">
+        <h2>공.장 나눔 공간</h2>
+      </div>
+      <ItemList/>
+    </MainBlock>
   );
 };
 
