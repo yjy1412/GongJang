59157f1d5fe2c61fd5e4b17d3b6dd7458b4fa339
treatme030 <treatme8192@gmail.com>
2021-11-26 10:26:17 +0900

    edit : header 컴포넌트 목업 하기 위해 header 컴포넌트 적용

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/common/Header';
 
 const Main = () => {
   return (
     <div>
-      Hello, world
+      <Header/>
     </div>
   );
 };
