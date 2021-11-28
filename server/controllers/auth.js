46bf7e9f81f1d02768101984f0e53be833864fde
yjy1412 <yjy852@gmail.com>
2021-11-28 21:05:54 +0900

    feat : 로그인 기능 구현

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,10 +5,10 @@ const jwt = require('jsonwebtoken');
 module.exports = {
   // POST auth/sign-up
   signup : (req, res) => {
-    // request connect test!
+    // connecting test
     console.log(req.body);
     
-    // input body data validation
+    // input data validation
     const inputEmail = req.body.email;
     const inputNickname = req.body.nickname;
     const inputPassword = req.body.password;
@@ -40,7 +40,55 @@ module.exports = {
   },
   // POST auth/log-in
   login : (req, res) => {
-    res.send('auth/login');
+    // connecting test
+    console.log(req.body);
+
+    // input data validation
+    const inputEmail = req.body.email;
+    const inputPassword = req.body.password;
+
+    if( !inputEmail || !inputPassword ) {
+      res.status(400).send("필수 입력요소가 누락되었습니다");
+    } else {
+      User.findOne({
+        where: {
+          email: inputEmail
+        }
+      })
+      .then(data => {
+        console.log(data);
+        if( !data ) {
+          res.status(404).send("입력하신 이메일 정보와 일치하는 회원정보가 없습니다")
+        } else {
+          if( data.password !== inputPassword ) {
+            res.status(404).send("비밀번호가 일치하지 않습니다")
+          } else {
+            const { email } = data.email;
+            const accessToken = jwt.sign(
+              { email },
+              process.env.ACCESS_SECRET,
+              { expiresIn: "1d" }
+            );
+            const refreshToken = jwt.sign(
+              { email },
+              process.env.REFRESH_SECRET,
+              { expiresIn: "30d" }
+            )
+            // token check
+            console.log(accessToken);
+            console.log(refreshToken);
+            
+            // response
+            res.cookie("refreshToken", refreshToken, {httpOnly: true, expiresIn: "30d"})
+            res.json({ accessToken })
+          }
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send("서버에 오류가 발생했습니다")
+      })
+    }
   },
   // POST auth/log-out
   logout : (req, res) => {
