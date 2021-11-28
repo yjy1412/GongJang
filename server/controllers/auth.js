dbbf4f34e42c0ac3025632779c07ee23c7af3065
yjy1412 <yjy852@gmail.com>
2021-11-28 22:58:13 +0900

    feat : 서버 회원정보 수정기능 구현

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
   // POST auth/sign-up
-  signup : (req, res) => {
+  signup : async (req, res) => {
     // connecting test
     console.log(req.body);
     
@@ -39,7 +39,7 @@ module.exports = {
     }
   },
   // POST auth/log-in
-  login : (req, res) => {
+  login : async (req, res) => {
     // connecting test
     console.log(req.body);
 
@@ -63,7 +63,8 @@ module.exports = {
           if( data.password !== inputPassword ) {
             res.status(404).send("비밀번호가 일치하지 않습니다")
           } else {
-            const { email } = data.email;
+            console.log("DATA : ", data);
+            const { email } = data.dataValues;
             const accessToken = jwt.sign(
               { email },
               process.env.ACCESS_SECRET,
@@ -91,27 +92,81 @@ module.exports = {
     }
   },
   // POST auth/log-out
-  logout : (req, res) => {
+  logout : async (req, res) => {
     res.send('auth/logout');
   },
   // DELETE auth/sign-out
-  signout : (req, res) => {
+  signout : async (req, res) => {
     res.send('auth/signout');
   },
   // GET auth/mypage
-  getMypage : (req, res) => {
+  getMypage : async (req, res) => {
     res.send('Get auth/mypage');
   },
   // GET auth/mypage/posts
-  getMyposts : (req, res) => {
+  getMyposts : async (req, res) => {
     res.send('Get auth/mypage/posts');
   },
   // PATCH auth/mypage
-  patchMypage : (req, res) => {
-    res.send('Patch auth/mypage');
+  patchMypage : async (req, res) => {
+    
+    // 1. 권한 확인
+    const authorization = req.headers.authorization;
+    console.log(authorization);
+
+    if ( !authorization ) {
+      res.status(401).send("권한인증에 실패했습니다(엑세스 토큰 부재)")
+    } else {
+      const accessToken = authorization.split(' ')[1];
+      console.log(accessToken);
+
+      let accessData;
+      try {
+        accessData = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+      } catch {
+        // 엑세스 토큰이 유효하지 않을 때
+        const refreshToken = req.cookies.refreshToken;
+        if ( refreshToken ) {
+          // 리프레쉬 토큰이 req.cookies에 담겨 있다면,
+          try {
+            // 리프레쉬 토큰을 통한 엑세스 데이터 임시추출
+            accessData = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+          } catch {
+            // 리프레쉬 토큰 마저 유효하지 않을 때(30일 경과 또는 토큰 훼손)
+            return res.status(401).send("권한인증에 실패했습니다(토큰 유효하지 않음)")
+          }
+        } else {
+          // 리프레쉬 토큰이 없다면 바로 에러메시지 전송
+          return res.status(401).send("권한인증에 실패했습니다(토큰 유효하지 않음)")
+        }
+      }
+
+      // 2. 입력 데이터 DB 반영
+      const inputNickname = req.body.nickname;
+      const inputProfileImage = req.body.profile_image;
+      const { email } = accessData;
+      console.log(accessData);
+
+      User.update({
+        nickname: inputNickname,
+        profile_image: inputProfileImage
+      }, {
+        where: {
+          email
+        }
+      })
+      .then(result => {
+        console.log(result);
+        res.status(201).send("회원정보가 수정되었습니다")
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send("서버에 오류가 발생했습니다")
+      })
+    }
   },
   // PATCH auth/password
-  patchPassword : (req, res) => {
+  patchPassword : async (req, res) => {
     res.send('Patch auth/password');
   }
 
