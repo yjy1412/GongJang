218b15796dbdddd91abbe01c4cc1c7203ca3a043
treatme030 <treatme8192@gmail.com>
2021-11-29 10:26:51 +0900

    edit : 스타일 코드 정리

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -10,23 +10,20 @@ const MainBlock = styled.div`
     position: relative;
     text-align: center;
     padding: 2rem 0;
-    &:after {
-      content: '';
-      position: absolute;
-      top: 50%;
-      left: 35%;
-      height: 2px;
-      width: 50px;
-      background: #575f95;
-    }
+    &:after,
     &:before {
       content: '';
       position: absolute;
       top: 50%;
-      right: 35%;
       height: 2px;
       width: 50px;
       background: #575f95;
+    }
+    &:after {
+      left: 35%;
+    }
+    &:before {
+      right: 35%;
     }
   }
 `;
