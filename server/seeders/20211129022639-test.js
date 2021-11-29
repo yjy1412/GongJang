58e63c0b077f81bedcb47cba727eb663ace9a463
Gwangandong <hansjimmy92@gmail.com>
2021-11-29 14:32:32 +0900

    feat : log-out 구현 중

---
+++ server/seeders/20211129022639-test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   return queryInterface.bulkInsert(
+     'Users',
+     [
+       {
+         email:'seedTest@test.com',
+         name : 'seed1',
+         password : 'asdf'
+
+       }
+     ]
+   )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
