507c58c30c5428bf8061311a2f8470a14aa14815
treatme030 <treatme8192@gmail.com>
2021-11-30 17:02:40 +0900

    feat : 나눔상태 컴포넌트 생성

---
+++ client/src/components/write/SalesStatus.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { changeState } from '../../feature/writeSlice';
+
+const SalesStatusBlock = styled.div`
+  position: relative;
+  input {
+    position: absolute;
+    top: 50%;
+    right: -70%;
+    transform: translateY(-50%);
+  }
+`;
+
+const SalesStatus = () => {
+  const dispatch = useDispatch();
+
+  const onCangeCheck = (e) => {
+    dispatch(changeState(e.target.checked));
+  }
+  
+  return (
+    <SalesStatusBlock>
+      <label for="status">
+      <input 
+      type="checkbox"
+      id="status" 
+      name="status"
+      onChange={onCangeCheck}
+      />
+      나눔상태</label>
+    </SalesStatusBlock>
+  );
+};
+
+export default SalesStatus;
