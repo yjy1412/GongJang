63e4b8ff1c8c96da38222f970e42c69ebd1902aa
treatme030 <treatme8192@gmail.com>
2021-12-06 09:49:36 +0900

    edit : 스타일 수정, 로그인 알림 모달 위치 변경

--- client/src/components/common/header/Header.js
+++ client/src/components/common/header/Header.js
@@ -5,17 +5,18 @@ import styled from 'styled-components';
 import MenuIcons from './MenuIcons';
 import Search from './Search';
 import { fetchLogOut } from '../../../feature/userSlice';
+import AskModal from '../../modal/AskModal';
+import HiddenMenu from './HiddenMenu';
 
 const HeaderBlock = styled.header`
     width: 100%;
-    height: 80px;
-    margin: 1rem 0;
+    height: 100px;
 `;
 
 const HeaderLayoutStyle = styled.div`
     width: 70%;
     height: 100%;
-    margin: 0 auto;
+    margin: 1rem auto;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -30,8 +31,8 @@ const HeaderLayoutStyle = styled.div`
             justify-content: center;
             gap: 1.5rem;
             p {
-            color: #c6c2c2;
-            font-weight: 600;
+                color: #c6c2c2;
+                font-weight: 600;
             }
             span {
                 color: #575F95;
@@ -83,6 +84,7 @@ const MenuBoxBlock = styled.div`
 const Header = () => {const dispatch = useDispatch();
 
     const [show, setShow] = useState(false);
+    const [visible, setVisible] = useState(false);
     const history = useHistory();
     const { isLogin, accessToken } = useSelector((state) => state.user);
     
@@ -96,6 +98,21 @@ const Header = () => {const dispatch = useDispatch();
             searchWrap.style.transition = 'transform .4s';
         }
     };
+
+    const handleModal = (e) => {
+        if(!isLogin){
+          setVisible(true);
+        }
+    }
+    
+    const onCancel = () => {
+        setVisible(false);
+    }
+
+    const onConfirm = () => {
+        setVisible(false);
+        history.push('/login')
+    }
     
     const handleLogOut = () => {
         if(accessToken && isLogin){
@@ -104,46 +121,61 @@ const Header = () => {const dispatch = useDispatch();
         if(!isLogin){
             history.push('/');
         }
-    }
-  
+    }    
+
     return (
-        <HeaderBlock>
-            <HeaderLayoutStyle>
-                <div className="space"></div>
-                <div className="logo">
-                    <Link to="/" className="logo-text">
-                        <div>
-                            <p><span>공</span> 유하는</p>
-                        </div>
-                        <div>
-                            <p><span>장</span> 난감</p>
-                         </div>
-                    </Link> 
-                </div>
-                <MenuBoxBlock className="menu-box">
-                    <ul className="auth">
-                        <li>
-                            { isLogin ? (
-                                <Link to="/" onClick={handleLogOut}>LOGOUT</Link>
-                            ) : (
-                                <Link to="/login">LOGIN</Link>
-                            )}
-                        </li>
-                        <li>
-                            <Link to="/join">JOIN</Link>
-                        </li>
-                    </ul>
-                    <MenuIcons
-                    onClick={onClick}
-                    />
-                </MenuBoxBlock>
-                <div id="search-wrap">
-                    <Search
-                    onClick={onClick}
-                    />
-                </div>
-            </HeaderLayoutStyle>
-        </HeaderBlock>
+        <>
+            <HeaderBlock>
+                <HeaderLayoutStyle>
+                    <div className="space"></div>
+                    <div className="logo">
+                        <Link to="/" className="logo-text">
+                            <div>
+                                <p><span>공</span> 유하는</p>
+                            </div>
+                            <div>
+                                <p><span>장</span> 난감</p>
+                            </div>
+                        </Link> 
+                    </div>
+                    <MenuBoxBlock className="menu-box">
+                        <ul className="auth">
+                            <li>
+                                { isLogin ? (
+                                    <Link to="/" onClick={handleLogOut}>LOGOUT</Link>
+                                ) : (
+                                    <Link to="/login">LOGIN</Link>
+                                )}
+                            </li>
+                            <li>
+                                <Link to="/join">JOIN</Link>
+                            </li>
+                        </ul>
+                        <MenuIcons
+                        onClick={onClick}
+                        handleModal={handleModal}
+                        isLogin={isLogin}
+                        />
+                    </MenuBoxBlock>
+                    <HiddenMenu/>
+                    <div id="search-wrap">
+                        <Search
+                        onClick={onClick}
+                        />
+                    </div>
+                </HeaderLayoutStyle>
+            </HeaderBlock>
+            { visible && (
+                <AskModal 
+                visible={visible}
+                title='알림'
+                description='로그인이 필요한 서비스입니다.'
+                addDescription='로그인 하시겠습니까?'
+                onConfirm={onConfirm}
+                onCancel={onCancel} 
+                /> 
+            )}
+        </>
     );
 };
 
