87a7073a416f9b05392fcc290154035c222a32b0
treatme030 <treatme8192@gmail.com>
2021-12-13 13:50:04 +0900

    feat : 스크롤 탑버튼 생성 및 기능구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Join from './pages/Join';
@@ -12,11 +12,67 @@ import Write from './pages/Write';
 import Header from './components/common/header/Header';
 import Footer from './components/common/Footer';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const TopBtnStyle = styled.div`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  margin-bottom: 3%;
+  margin-right: 3%;
+  width: 50px;
+  height: 50px;
+  z-index: -10;
+  opacity: 0;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  background: rgba(218, 220, 236, 0.5);
+  cursor: pointer;
+  transition: opacity .3s;
+  span {
+    width: 100%;
+  }
+  &.active {
+    z-index: 10;
+    opacity: 1;
+  }
+`;
 
 function App() {
-  
   const { accessToken } = useSelector( state => state.user);
   axios.defaults.headers.common['authorization'] = `${accessToken ? `Bearer ${accessToken}` : ''}`;
+
+  const [windowScrollY, setWindowScrollY] = useState(0);
+  const [btnState, setBtnState] = useState(false);
+
+  const onScrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+    setWindowScrollY(0);
+    setBtnState(false);
+  }
+
+  useEffect(() => {
+    const scrollHandle = () => {
+      setWindowScrollY(window.scrollY);
+      if(windowScrollY > 100){
+        setBtnState(true);
+      } else {
+        setBtnState(false);
+      }
+    }
+    const move = () => {
+      window.addEventListener('scroll', scrollHandle);
+    }
+    move();
+    return () => {
+      window.removeEventListener('scroll', scrollHandle);
+    }
+  },[windowScrollY])
+
   return (
     <>
       <Header/>
@@ -31,6 +87,9 @@ function App() {
         <Route component={PostDetail} exact path="/:id"/>
       </Switch>
       <Footer/>
+      <TopBtnStyle className={btnState ? 'active' : ''} onClick={onScrollTop}>
+        <span><b>Top</b></span>
+      </TopBtnStyle>
     </>
   );
 }
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -96,6 +96,7 @@ const Main = () => {
         modal={modal}
         setModal={setModal}
         />
+        
       </MainBlock>
       { modal && (
         <AskModal 
