325b76c1e91ff44799ddb988eefe05b5781cd8f2
yjy1412 <yjy852@gmail.com>
2021-12-15 15:15:15 +0900

    edit : 초기 배포설정

--- server/config/config.js
+++ server/config/config.js
@@ -12,20 +12,20 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.DATABASE_USER_TEST,
-    "password": process.env.DATABASE_PASSWORD_TEST,
-    "database": process.env.DATABASE_NAME_TEST,
-    "host": process.env.DATABASE_HOST_TEST,
-    "port": process.env.DATABASE_PORT_TEST,
-    "timezone": "+09:00",
-    "dialect": "mysql"
-  },
-  "production": {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
     "host": process.env.DATABASE_HOST,
     "port": process.env.DATABASE_PORT,
+    "timezone": "+09:00",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.PRODUCT_DATABASE_USER,
+    "password": process.env.PRODUCT_DATABASE_PASSWORD,
+    "database": process.env.PRODUCT_DATABASE_NAME,
+    "host": process.env.PRODUCT_DATABASE_HOST,
+    "port": process.env.PRODUCT_DATABASE_PORT,
     "timezone": "+09:00",
     "dialect": "mysql"
   }
