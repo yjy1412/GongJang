25ce04e2bed3e76d203b44e1cc1b04962d0c716f
djdu4496 <djdu4496@gmail.com>
2021-12-15 17:24:48 +0900

    edit : 사용하지 않는 함수 제거

--- client/src/components/mypage/MyPageProfileImg.js
+++ client/src/components/mypage/MyPageProfileImg.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { changeWishPost } from '../../feature/postSlice';
 import { changeProfileImage, fetchUpdateProfileImage } from '../../feature/userSlice';
 import AskEditModal from '../modal/AskEditModal';
 
