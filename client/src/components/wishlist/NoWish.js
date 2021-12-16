86be8d297853130ee96dc44a1f1cb194531763f9
djdu4496 <djdu4496@gmail.com>
2021-12-16 11:12:34 +0900

    feat : 위시리스트가 존재하지 않을 경우 메시지 구현

---
+++ client/src/components/wishlist/NoWish.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BsFillEmojiFrownFill } from 'react-icons/bs'
+const NoWish = () => {
+
+  const NoResultBlock = styled.div`
+  height: 80vh;
+  position: relative;
+  .no-result-box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 600px;
+    height: 200px;
+    min-width: fit-content;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .no-result-icon { 
+    font-size: 150px;
+  }
+
+  .no-result-message {
+    margin-top: 30px;
+    font-size: 25px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    
+    span {
+      text-align: center;
+    }
+  }
+`;
+
+  return (
+    <NoResultBlock>
+      <div className="no-result-box">
+        <BsFillEmojiFrownFill className="no-result-icon"/>
+        <div className="no-result-message ">
+          <span>위시리스트가 존재하지 않습니다.</span>
+        </div>
+      </div>
+    </NoResultBlock>
+  );
+};
+
+export default NoWish;
