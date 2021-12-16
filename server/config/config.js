56818289db53f6bce3d0ba424bf02ccf5c5f6c09
Ubuntu <ubuntu@ip-172-31-42-66.ap-northeast-2.compute.internal>
2021-12-16 02:04:20 +0000

    edit : 데이터베이스 설정

--- server/config/config.js
+++ server/config/config.js
@@ -21,11 +21,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
-    "port": process.env.DATABASE_PORT,
+    "username": process.env.PRODUCT_DATABASE_USER,
+    "password": process.env.PRODUCT_DATABASE_PASSWORD,
+    "database": process.env.PRODUCT_DATABASE_NAME,
+    "host": process.env.PRODUCT_DATABASE_HOST,
+    "port": process.env.PRODUCT_DATABASE_PORT,
     "timezone": "+09:00",
     "dialect": "mysql"
   }
--- server/server.js
+++ server/server.js
@@ -6,11 +6,18 @@ const cors = require('cors');
 
 const router = require('./routers')
 const app = express();
+const NODE_ENV = process.env.NODE_ENV;
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
+const PRODUCT_CLIENT_ORIGIN = process.env.PRODUCT_CLIENT_ORIGIN;
+let origin = CLIENT_ORIGIN;
+if ( NODE_ENV === 'production' ) {
+  origin = PRODUCT_CLIENT_ORIGIN
+}
 
 app.use(express.json())
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: origin,
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
   })
