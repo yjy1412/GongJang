62dc8c5bde240ccd342eba20d58c1e2ebe1201e0
djdu4496 <djdu4496@gmail.com>
2021-12-17 12:32:51 +0900

    edit : 캐러셀 반응형 웹 적용 & 텍스트 삽입 위한 스타일 수정 & JSX 수정

--- client/src/components/main/CarouselImg.js
+++ client/src/components/main/CarouselImg.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-const CarouselImg = ({ image }) => {
+const CarouselImg = ({ image, text, key }) => {
 
   const carouselStyles={
-    width: 100+"%",
+    width: 50+"%",
     height: 100+"%",
   }
 
-  return <img src={image} alt="carousel-img" style={carouselStyles}/>
+  return (
+  <>
+    <div className="carousel-text">
+      <div className="carousel-text-first">
+        {text[0]}
+      </div>
+      <div className="carousel-text-second">
+        {text[1]}
+      </div>
+      <div className="carousel-text-third">
+        {text[2]}
+      </div>
+    </div>
+    <img src={image} alt="carousel-img" style={carouselStyles}/>
+  </>
+  )
 };
 
 export default CarouselImg;
